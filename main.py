6df7d21bb36aee626012df7cc6b47176643a9e3e
sosthenes-py <sosthenes.wedigital@gmail.com>
2023-08-08 19:06:06 +0100

    Removed explicit db instance path

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 
-app = Flask(__name__, instance_path="C:/Users/sosso/PycharmProjects/day-70-starting-files-blog-for-deployment/instance")
+app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']
 ckeditor = CKEditor(app)
 Bootstrap5(app)
